e42e58d8d9d3b1ad8a460ba066cede3a47007b7a
barisakyildiz <barisakyildiz3@gmail.com>
2022-12-12 18:30:38 +0300

    solved hover

--- test.py
+++ test.py
@@ -1,12 +1,13 @@
+import selenium
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
 from time import sleep
 import pickle
-import json
+from selenium.webdriver.common.by import By
 
 class Driver:
     def __init__(self, url):
-        self.url = "https://www.wildberries.ru"
+        self.url = "https://www.wildberries.ru/catalog/16429349/detail.aspx?"
         self.PICKLES_PATH = 'pic.pkl'
         options = Options()
         options.headless = False
@@ -15,14 +16,32 @@ class Driver:
         options.add_argument('--disable-dev-shm-usage')
         DRIVER_PATH = '/home/da3rny/Tornacı/priceChecker/chromedriver'
         self.driver = webdriver.Chrome(executable_path=DRIVER_PATH, options=options)
+        self.action = webdriver.ActionChains(self.driver)
         self.driver.get(self.url)
         sleep(3)
         self.loadCookies(self.driver)
         self.driver.get(self.url)
-        self.driver.maximize_window(); sleep(200)
-        self.dumpCookies(self.driver)
+        self.driver.maximize_window()
+        sleep(5)
+        self.element = self.driver.find_elements(by=By.CLASS_NAME, value='product-page__price-block')
+        print("------------------------------------")
+        print(self.element)
+        print("------------------------------------")
+        for element in self.element:
+            try:
+                self.action.move_to_element(element)
+                self.action.perform()
+                sleep(1)
+            except Exception as e:
+                print("NO SIZE AND SMT" + str(e))
+                sleep(1)
+        sleep(5)
         self.html = self.driver.page_source
-        print(self.html)
+        #print(self.html)
+        print("------------------------------------")
+        where = self.html.find("Скидка покупателя")
+        print(self.html[where:where+60])
+        print("------------------------------------")
 
     def dumpCookies(self, driver):
         with open(self.PICKLES_PATH, 'wb') as filehandler:
