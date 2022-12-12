aa43919226bb2171cdf34d3791d240a32c6041f9
da3rny <barisakyildiz3@gmail.com>
2022-12-12 15:07:16 +0300

    tested on cookies

--- test.py
+++ test.py
@@ -1,20 +1,44 @@
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
 from time import sleep
+import pickle
+import json
 
 class Driver:
     def __init__(self, url):
-        self.url = "https://www.wildberries.ru/catalog/16429349/detail.aspx?"
+        self.url = "https://www.stackoverflow.com"
+        self.PICKLES_PATH = 'pic.pkl'
         options = Options()
-        options.headless = True
+        options.headless = False
         options.page_load_strategy = 'eager'
         options.add_argument('--no-sandbox')
         options.add_argument('--disable-dev-shm-usage')
-        DRIVER_PATH = 'chromedriver.exe'
+        DRIVER_PATH = '/home/da3rny/Tornacı/priceChecker/chromedriver'
         self.driver = webdriver.Chrome(executable_path=DRIVER_PATH, options=options)
+        self.loadCookies(self.driver)
+        sleep(3)
         self.driver.get(self.url)
-        self.driver.maximize_window(); sleep(5)
+        self.driver.maximize_window(); sleep(10)
+        self.dumpCookies(self.driver)
         self.html = self.driver.page_source
         print(self.html)
+
+    def dumpCookies(self, driver):
+        with open(self.PICKLES_PATH, 'wb') as filehandler:
+            pickle.dump(driver.get_cookies(), filehandler)
     
+    def loadCookies(self, driver):
+     with open(self.PICKLES_PATH, 'rb') as cookiesfile:
+         cookies = pickle.load(cookiesfile)
+         for cookie in cookies:
+            del cookie['domain']
+            del cookie['expiry']
+            del cookie['httpOnly']
+            del cookie['domain']
+            print(cookie)
+            driver.add_cookie(cookie)
+    
+import sys
+import os
+
 dri = Driver("difugh")
